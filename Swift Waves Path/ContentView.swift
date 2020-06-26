9ff6fd6c6c07f3a0d0220532e1038e20a9f2e1e2
Euler Alvarenga <euler.sistemas@gmail.com>
2020-06-26 14:45:25 -0300

    first commit

---
+++ Swift Waves Path/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Waves Path
+//
+//  Created by Euler Alvarenga on 26/06/20.
+//  Copyright © 2020 Euler Alvarenga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Waves PathTests/Swift_Waves_PathTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_Waves_PathTests.swift
+//  Swift Waves PathTests
+//
+//  Created by Euler Alvarenga on 26/06/20.
+//  Copyright © 2020 Euler Alvarenga. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Waves_Path
+
+class Swift_Waves_PathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
